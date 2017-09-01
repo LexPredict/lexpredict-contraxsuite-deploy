4e213f0542e206c7c8deb640f728e60678b43a2d
alex <reddalexx@gmail.com>
2017-09-01 14:51:39 +0300

    1.0.1 release changes

--- fabfile.py
+++ fabfile.py
@@ -24,10 +24,10 @@ from fabtools.postgres import (create_database,
 
 __author__ = "ContraxSuite, LLC; LexPredict, LLC"
 __copyright__ = "Copyright 2015-2017, ContraxSuite, LLC"
-__license__ = "https://github.com/LexPredict/lexpredict-contraxsuite/blob/1.0/LICENSE.pdf"
-__version__ = "1.0"
+__license__ = "https://github.com/LexPredict/lexpredict-contraxsuite/blob/1.0.1/LICENSE"
+__version__ = "1.0.1"
 __maintainer__ = "LexPredict, LLC"
-__email__ = "support@lexpredict.com"
+__email__ = "support@contraxsuite.com"
 
 
 """
